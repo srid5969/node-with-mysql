480d580c5b431bf8d3c448906b5fb1f94438fe20
Sridhar <sridharofficialmail@gmail.com>
2023-06-08 15:40:41 +0530

    Refactor server.ts file to call main function from app.ts
    - Refactored src/server.ts file to remove duplicated code by importing main function from src/app/app.ts. This allowed us to clean up server.ts and abstract the creation of the server instance.
    - Consisted of updating import statements and changing the app instance to an express Application type, and calling main function from app.ts.
    
    This commit improves the modularity and maintainability of the code.

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
 import "module-alias/register";
-import { userRouter } from "app/user/router/user";
-import bodyParser from "body-parser";
-import { middleware } from "common/ErrorHandler/afterMiddleWare";
-import cors from "cors";
-import dotenv from "dotenv";
-import express, { Express } from "express";
-import morgan from "morgan";
+import { main } from "app/app";
+import express, { Application } from "express";
 
-if (dotenv.config().error) throw Error("Cannot find dot env file");
-const port: number = Number(process.env.PORT) || 8080;
-
-const app: Express = express();
-app.use(cors({ origin: "*" }));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.json());
-app.use(morgan("short"));
-const baseUrl: string = process.env.API_PREFIX || "";
-
-app.use(baseUrl, userRouter);
-app.use(middleware);
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+const app: Application = express();
+main(app).then(() => {
+  console.log("up and running");
 });
