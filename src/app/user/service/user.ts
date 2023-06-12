d15ceafb1ab898fdaa605ff8692db60572bad190
Sridhar <sridharofficialmail@gmail.com>
2023-06-12 10:16:04 +0530

    Update dependencies and add Sequelize database ORM library - Update @types/node to version 20.2.6 from 20.2.5 - Add sequelize dependency and initialize with logging and data source configuration - Update User model with TypeORM annotations - Replace database connection string with Sequelize connection and import sequelize in app.ts - Update save user and list users functions to use TypeORM repository instead of mssql - Update delete user and get user by id functions to use TypeORM findOneBy method - Add AppDataSource variable to hold the initialized TypeORM DataSource connection in database.ts - Update server.ts file to initialize AppDataSource and start listener after initializing the data source

--- src/app/user/service/user.ts
+++ src/app/user/service/user.ts
@@ -1,23 +1,21 @@
 import { User } from "app/user/model/user";
-import sql, { query } from "mssql";
-import { deleteUserQuery, editUserQuery, getAllUsers, getUserByIdQuery, insertUser } from "resources/query/user/user";
+import { AppDataSource } from "configuration/database";
+import sql from "mssql";
+import { deleteUserQuery, editUserQuery, getUserByIdQuery, insertUser } from "resources/query/user/user";
+const userRepository = AppDataSource.getRepository(User);
 
 export async function saveUser(data: User) {
-  const request = new sql.Request();
-  request.input("id", sql.VarChar, data.id);
-  request.input("name", sql.VarChar, data.name);
-  request.input("password", sql.VarChar, data.password);
-
-  return await request.query(insertUser).catch((e) => e);
+  return await userRepository.save(data);
 }
 export async function listUsers(): Promise<any> {
-  return await query(getAllUsers).catch((e) => e);
+  return await userRepository.find().catch((e) => e);
 }
 export async function editUserName(id: string, data: User): Promise<any> {
-  const request = new sql.Request();
-  request.input("name", sql.VarChar, data.name);
-  request.input("id", sql.VarChar, id);
-  return await request.query(editUserQuery).catch((e) => e);
+  const user:any=userRepository.findOneBy({
+    id: Number(id),
+  });
+  user.name=''
+  return await userRepository.save(data).catch((e) => e);
 }
 export async function deleteUser(id: string): Promise<any> {
   const request = new sql.Request();
@@ -25,7 +23,7 @@ export async function deleteUser(id: string): Promise<any> {
   return await request.query(deleteUserQuery).catch((e) => e);
 }
 export async function getUserById(id: string): Promise<any> {
-  const request = new sql.Request();
-  request.input("id", sql.VarChar, id);
-  return await request.query(getUserByIdQuery).catch((e) => e);
+  return await userRepository.findOneBy({
+    id: Number(id),
+  });
 }
--- src/configuration/database.ts
+++ src/configuration/database.ts
@@ -1,13 +1,26 @@
+import { User } from "app/user/model/user";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 if (dotenv.config().error) throw Error("Cannot find ");
-const configuration :any= {
-  server: process.env.MSSQL_SERVER || "",
-  user: process.env.MSSQL_USERNAME || "",
+const configuration: any = {
+  type: "mssql",
+  host: "localhost",
+  port: 1433,
+  username: process.env.MSSQL_USERNAME || "",
   password: process.env.MSSQL_PASSWORD || "",
   database: process.env.MSSQL_DATABASE || "",
-  trustServerCertificate: true    
-}
+  extra: {
+    trustServerCertificate: true,
+  },
+
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  subscribers: [],
+  migrations: [],
+};
 export default configuration;
- console.log(configuration);
- 
+
+export const AppDataSource = new DataSource(configuration);
+AppDataSource.initialize().then(() => console.log("success"));
